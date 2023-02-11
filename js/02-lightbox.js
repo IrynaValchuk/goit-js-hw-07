ef37bb0b54397803e5902bf1798a35c0415165de
Iryna <irula17ooo@gmail.com>
2023-02-11 23:54:54 +0200

    add task-2.1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+const createImagesGallery = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li><a class="gallery__item" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}" 
+          />
+        </a>
+      </li>`
+  )
+  .join('');
+
+galleryList.insertAdjacentHTML('afterbegin', createImagesGallery);
